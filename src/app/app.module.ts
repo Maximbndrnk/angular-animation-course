e6407221a65f1cc48dae437caa09e31f2f9d064d
Maxim Bondarenko <maximbndrnk@gmail.com>
2020-03-18 09:51:21 +0200

    add basic files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './home/home.component';
+import { BasicComponent } from './basic/basic.component';
+import { AdvacedComponent } from './advaced/advaced.component';
+import { ContainedComponent } from './contained/contained.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    BasicComponent,
+    AdvacedComponent,
+    ContainedComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
